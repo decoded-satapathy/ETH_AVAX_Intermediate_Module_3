b8525de7d42ae325bba492e5c23f0217341ddc94
Om Satapathy <115103314+decoded-satapathy@users.noreply.github.com>
2024-06-13 23:12:53 +0530

    Create LinuxEduToken.sol

---
+++ LinuxEduToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract LinuxEduToken is Ownable, ERC20Burnable {
+    constructor(uint256 initialSupply) ERC20("LinuxEDU", "LNX") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mintToken(address _toAddress, uint _amount) public onlyOwner {
+        _mint(_toAddress, _amount);
+    }
+
+    function burnToken(uint _amount) public {
+        _burn(msg.sender, _amount);
+    }
+
+    function transferToken(address _toAddress, uint _amount) public {
+        bool sucess = transfer(_toAddress, _amount);
+        assert(sucess);
+    }
+}
